c1e470db198c2b79db6d6a8e18914644fb8d7f29
chisom <chukwukweluchisom@gmail.com>
2022-03-12 17:15:39 -0800

    c language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return
+ */
+int main(void)
+{
+printf('size of a char: %d byte(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+return (0);
+}
