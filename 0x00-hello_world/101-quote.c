e2df9e6c931967211061c469d8946a1ab2033cc5
chisom <chukwukweluchisom@gmail.com>
2022-03-14 18:50:07 -0700

    print witout printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 8);
+fwrite(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 8);
 
 return 1;
 
