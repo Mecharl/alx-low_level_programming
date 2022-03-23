47a7440b76199c29301810ce7861328257a07341
chisom <chukwukweluchisom@gmail.com>
2022-03-23 05:03:31 -0700

    task num 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * rev_string - prints a reverse string
+ * @s: A pointer to an int that will be changed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+char *starts_C, *end_C, C;
+inti, count;
+int length = 0;
+
+for (i = 0; s[i]; i++)
+{
+length++;
+}
+
+count = length;
+
+start_C = s;
+end_C = s;
+
+for (i = 0; i < count - 1; i++)
+{
+end_C++;
+}
+
+for (i = 0; i < count / 2; i++)
+{
+
+c = *end_C;
+*end_C = *start_C;
+*start_C = C;
+
+
+start_C++;
+end_C--;
+}
+}
